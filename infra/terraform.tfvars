554088baff614d1d9dda81c0b9f5511bb1e1f885
ABSQUARE1 <yusufolatuji53@gmail.com>
2023-05-04 13:42:39 +0000

    added variables and tfvar

---
+++ infra/terraform.tfvars
@@ -0,0 +1,13 @@
+name_prefix = "zenith"
+
+instance_type = "t2.micro"
+
+key_name = "myec2"
+
+max_size = 6
+
+min_size = 2
+
+desired_capacity = 2
+
+region = "us-east-1"
---
+++ infra/variable.tf
@@ -0,0 +1,21 @@
+variable "name_prefix" {
+    type = string
+}
+variable "instance_type" {
+    type = string
+}
+variable "key_name" {
+    type = string
+}
+variable "max_size" {
+    type = number
+}
+variable "min_size" {
+    type = number
+}
+variable "desired_capacity" {
+    type = number
+}
+variable "region" {
+    type = string
+}
